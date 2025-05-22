8b56939a1d65424a05b85733abe9f9a4759ac844
jarukit <jarukitatwork@gmail.com>
2025-05-22 11:21:08 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  output: "export",
+  basePath: "/Bigjarukit02.github.io",
+  assetPrefix: "/Bigjarukit02.github.io/",
+
   // basePath: '/next-project',
   /* config options here */
 };
